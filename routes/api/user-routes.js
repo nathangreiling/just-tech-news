eb0dcb9fa33a25b8ff242383af807f518e2cba3a
nathangreiling <ngreiling20@gmail.com>
2022-05-03 16:47:43 -0500

    post

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,33 +2,45 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 // GET /api/users
-User.findAll({
-  attributes: { exclude: ['password'] }
-})
-  .then(dbUserData => res.json(dbUserData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+router.get('/', (req, res) => {
+  User.findAll({
+    attributes: { exclude: ['password'] }
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 
 // GET /api/users/1
-User.findOne({
-  attributes: { exclude: ['password'] },
-  where: {
-    id: req.params.id
-  }
-})
-  .then(dbUserData => {
-    if (!dbUserData) {
-      res.status(404).json({ message: 'No user found with this id' });
-      return;
-    }
-    res.json(dbUserData);
+router.get('/:id', (req, res) => {
+  User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'post_url', 'created_at']
+      }
+    ]
   })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 
 // POST /api/users
 router.post('/', (req, res) => {
